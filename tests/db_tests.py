54c924dc9cf3f6a0249fa1c565e242f1c65f12e6
Jeffrey Meyers <meyers2@pdx.edu>
2015-12-30 14:56:57 -0800

    add some more tests for ranking different node types

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -15,6 +15,16 @@ sys.path.append(root)
 from app import app, graph
 
 
+class Exists(object):
+    """
+    these nodes must exist in the neo4j instance you are running tests against
+    """
+    ISSUE = "i1"
+    VALUE = "v1"
+    OBJECTIVE = "o1"
+    POLICY = "p1"
+
+
 class UserTests(unittest.TestCase):
 
     def setUp(self):
@@ -63,7 +73,6 @@ class UserTests(unittest.TestCase):
 
         # clean up
         graph.delete_user(self.user)
-
        
     def test_get_user(self):
         # make sure user exists
@@ -87,40 +96,64 @@ class UserTests(unittest.TestCase):
 
 class UserRankTests(unittest.TestCase):
     
+    DEFAULT_RANK = 1
+
     def setUp(self):
         self.app = app.test_client()
-        self.endpoint = "/api/value"
         
+        # test defaults
+        self.default_endpoint = "/api/value/rank"
+        self.node_type = "Value"
+        self.node = Exists.VALUE
+
         # make sure user exists
         self.user = "testuser-{0}".format(uuid.uuid4())
-        self.node = "v1"
-        self.rank1 = 1
-        self.rank2 = 2
         self.user_data = dict(username=self.user, name="Test", city="Portland")
         graph.create_user(self.user_data)
-        self.data = dict(user_id="", node_id="", rank=self.rank1, issue_id='i1')
+        self.data = dict(
+            user_id=self.user,
+            rank=self.DEFAULT_RANK,
+            issue_id=Exists.ISSUE
+        )
 
     def tearDown(self):
         user = graph.find_node("User", self.user)
-        node = graph.find_node("Value", self.node)
+        node = graph.find_node(self.node_type, self.node)
         graph.delete_link(user, node, "RANKS")
         graph.delete_user(self.user)
 
-    def test_valid_rank(self):
+    def valid_rank_runner(self, node_type, node):
+        endpoint = os.path.join("/api", node_type.lower(), "rank")
         data = dict(self.data)
-        data["user_id"] = self.user
-        data["node_id"] = self.node
-        rv = self.app.post(self.endpoint, data=data)
+        self.node_type = node_type
+        self.node = data["node_id"] = node
+        rv = self.app.post(endpoint, data=data)
         expected = dict(success=True)
         self.check_status_code(rv.status_code)
         self.check_response(expected, rv.data)
-        self.check_rank(self.rank1)
+        self.check_rank(self.DEFAULT_RANK)
+
+    def test_rank_issue(self):
+        self.data = dict(
+            user_id=self.user,
+            rank=self.DEFAULT_RANK
+        )
+        self.valid_rank_runner("Issue", Exists.ISSUE)
+
+    def test_rank_value(self):
+        self.valid_rank_runner("Value", Exists.VALUE)
+    
+    def test_rank_objective(self):
+        self.valid_rank_runner("Objective", Exists.OBJECTIVE)
+    
+    def test_rank_policy(self):
+        self.valid_rank_runner("Policy", Exists.POLICY)
 
     def test_invalid_rank_no_user(self):
         data = dict(self.data)
         data["user_id"] = uuid.uuid4()
         data["node_id"] = self.node 
-        rv = self.app.post(self.endpoint, data=data)
+        rv = self.app.post(self.default_endpoint, data=data)
         expected = dict(success=False, error="invalid user_id")
         self.check_status_code(rv.status_code)
         self.check_response(expected, rv.data)
@@ -130,7 +163,7 @@ class UserRankTests(unittest.TestCase):
         data = dict(self.data)
         data["user_id"] = self.user
         data["node_id"] = uuid.uuid4()
-        rv = self.app.post(self.endpoint, data=data)
+        rv = self.app.post(self.default_endpoint, data=data)
         expected = dict(success=False, error="invalid node_id")
         self.check_status_code(rv.status_code)
         self.check_response(expected, rv.data)
@@ -139,26 +172,28 @@ class UserRankTests(unittest.TestCase):
         # rank a node then update the ranking
         data = dict(self.data)
         data["user_id"] = self.user
-        data["node_id"] = self.node 
+        data["node_id"] = self.node
         
         # rank node initially
-        rv = self.app.post(self.endpoint, data=data)
+        rv = self.app.post(self.default_endpoint, data=data)
         self.check_status_code(rv.status_code)
         expected = dict(success=True)
         self.check_response(expected, rv.data)
-        self.check_rank(self.rank1)
+        self.check_rank(self.DEFAULT_RANK)
+
+        new_rank = self.DEFAULT_RANK + 1
 
         # update rank
-        data["rank"] = self.rank2
-        rv = self.app.post(self.endpoint, data=data)
+        data["rank"] = new_rank
+        rv = self.app.post(self.default_endpoint, data=data)
         self.check_status_code(rv.status_code)
         expected = dict(success=True)
         self.check_response(expected, rv.data)
-        self.check_rank(self.rank2)
+        self.check_rank(new_rank)
 
     def check_rank(self, rank):
         user = graph.find_node("User", self.user)
-        node = graph.find_node("Value", self.node)
+        node = graph.find_node(self.node_type, self.node)
         assert user and node, "failed to find node and/or user"
         link = graph.find_link(user, node, "RANKS")
         assert link, "failed to find link"
@@ -174,11 +209,6 @@ class UserRankTests(unittest.TestCase):
 
 
 class GetNodeTests(unittest.TestCase):
-
-    issue = "i1"
-    value = "v1"
-    objective = "o1"
-    policy = "p1"
 
     def setUp(self):
         self.app = app.test_client()
@@ -200,16 +230,16 @@ class GetNodeTests(unittest.TestCase):
         return response
 
     def test_get_issue(self):
-        self.get_runner("/api/issue", self.issue)
+        self.get_runner("/api/issue", Exists.ISSUE)
     
     def test_get_value(self):
-        self.get_runner("/api/value", self.value)
-     
-    def test_get_policy(self):
-        self.get_runner("/api/policy", self.policy)
+        self.get_runner("/api/value", Exists.VALUE)
   
     def test_get_objective(self):
-        self.get_runner("/api/objective", self.objective)
+        self.get_runner("/api/objective", Exists.OBJECTIVE)
+    
+    def test_get_policy(self):
+        self.get_runner("/api/policy", Exists.POLICY)
 
     def tearDown(self):
         # cleanup testuser
@@ -218,6 +248,3 @@ class GetNodeTests(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
