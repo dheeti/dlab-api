30bc34fc25cce4f3384196a0cf9fbf949ba19594
uym2 <ceciliamaitouyen@gmail.com>
2016-01-25 02:35:41 -0500

    Update graph_node_tests.py
    Add user, rank, and more value nodes. Add test for findall_withUserID( )

--- tests/integration_tests/graph_node_tests.py
+++ tests/integration_tests/graph_node_tests.py
@@ -9,18 +9,29 @@ root = dirname(dirname(dirname(abspath(__file__))))
 sys.path.append(root)
 
 from app import graph
-from shared.neo_utils import Handler, Node, Link
+from shared.neo_utils import Handler, Node, Link, User, Rank
 
 
 class NodesFindAllTest(unittest.TestCase):
 
     def setUp(self):
+        """
+        Tests for findall() and findall_withUserID() 
+        Create 1 user, 1 issue and 5 value nodes
+        User ranks values 0, 3, 4
+        Issue has values 0, 1, 4
+        """
         self.handler = Handler()
         self.issue = Node(self.handler, "TestIssue")
-        self.nodes = [ Node(self.handler, "TestValue") for i in range(0, 3) ]        
+        self.nodes = [ Node(self.handler, "TestValue") for i in range(0, 5) ]
+        self.user = User(self.handler)
         Link(self.handler, self.issue.node, self.nodes[0].node, "HAS") 
-        Link(self.handler, self.issue.node, self.nodes[1].node, "HAS") 
-
+        Link(self.handler, self.issue.node, self.nodes[1].node, "HAS")
+        Link(self.handler, self.issue.node, self.nodes[4].node, "HAS")		
+        Rank(self.handler, self.user.node, self.nodes[0].node, 1)
+        Rank(self.handler, self.user.node, self.nodes[3].node, 2)
+        Rank(self.handler, self.user.node, self.nodes[4].node, -1)
+		
     def tearDown(self):
         self.handler.clean_up()
 
@@ -35,16 +46,30 @@ class NodesFindAllTest(unittest.TestCase):
 
     def test_parent_issue_findall(self):
         """
-        test that only first two TestValue nodes are returned as the last node does
+        test that only TestValue nodes 0, 1, 4 are returned as the other nodes do
         not have a link from the TestIssue node
         """
-        node_ids = [self.nodes[0].node_id, self.nodes[1].node_id]
+        nodeIn_ids = [self.nodes[0].node_id, self.nodes[1].node_id, self.nodes[4].node_id]
+        nodeNotIn_ids = [self.nodes[2].node_id, self.nodes[3].node_id]
         args = dict(parent_id=self.issue.node_id, parent_label="TestIssue")
         nodes = graph.nodes.find_all("TestValue", **args)
         for node in nodes:
-            self.assertIn(node["node_id"], node_ids)
-            self.assertNotEqual(node["node_id"], self.nodes[2].node_id)
+            self.assertIn(node["node_id"], nodeIn_ids)
+            self.assertNotEqual(node["node_id"], nodeNotIn_ids)
 
-
+    def test_parent_issue_findall_withUserID(self):
+        """
+        test that only TestValue 2 nodes 0, 4 are returned as the other nodes do
+        not have a link from the TestIssue node or are not ranked by the user
+        """
+        nodeIn_ids = [self.nodes[0].node_id, self.nodes[4].node_id]
+        nodeNotIn_ids = [self.nodes[1].node_id, self.nodes[2].node_id, self.nodes[3].node_id]
+        args = dict(parent_id=self.issue.node_id, parent_label="TestIssue")
+        nodes = graph.nodes.find_all_withUserID("TestValue", self.user.node_id,**args)
+        for node in nodes:
+            self.assertIn(node["node_id"], nodeIn_ids)
+            self.assertNotEqual(node["node_id"], nodeNotIn_ids)
+		
+			
 if __name__ == '__main__':
     unittest.main()
