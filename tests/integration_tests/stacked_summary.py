18b4c29cfa9728957dbca9eb66643d08ea448e3c
Jeffrey Meyers <meyers2@pdx.edu>
2016-02-14 09:39:29 -0800

    make assertions python3 compatible:

--- tests/integration_tests/stacked_summary.py
+++ tests/integration_tests/stacked_summary.py
@@ -2,12 +2,13 @@ import sys
 import json
 from os.path import dirname, abspath
 import unittest
+from six import iteritems
 
 # add parent directory to path to allow importing app
 root = dirname(dirname(dirname(abspath(__file__))))
 sys.path.append(root)
 
-
+from shared import safe_json_loads
 from app import app, graph
 
 
@@ -28,9 +29,9 @@ class GetSummaryTests(unittest.TestCase):
     def test_valid(self):
         rv = self.app.get(self.endpoint, data=self.parameters())
         self.check_status_code(rv.status_code)
-        response = json.loads(rv.data)
+        response = safe_json_loads(rv.data)
         self.assertTrue(response["success"], msg="success not true")
-        for key, value in response["data"].iteritems():
+        for (key, value) in iteritems(response["data"]):
             self.assertIs(len(value["data"]), 5)
 
 
